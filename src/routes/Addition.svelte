01b139e8b99f1efc812ffb9680094bc4665785be
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2024-03-20 16:38:31 +0530

    added an additional function

---
+++ src/routes/Addition.svelte
@@ -0,0 +1,12 @@
+<script>
+    let num1 = 0;
+    let num2 = 0;
+</script>
+
+<diV>
+    <p><u>Enter 2 numbers to add the numbers and display the result</u></p>
+    
+    Number 1 : <input type="number" bind:value={num1}/> <input type="range" bind:value={num1}>
+    Number 2 : <input type="number" bind:value={num2}/> <input type="range" bind:value={num2}>
+    Result : {num1 + num2}
+</diV>
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,17 @@
+<script>
+import { createEventDispatcher } from "svelte";
+
+export let nested_var = 0;
+
+const dispatch = createEventDispatcher();
+
+function sayBooyah() {
+    dispatch('message',{
+        text: 'Booyah'
+    });
+}
+</script>
+
+<p> Nested Component is {nested_var}</p>
+
+<button on:click={sayBooyah}> Booyah </button>
